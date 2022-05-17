4331e92134818c24a2d5c561d9158996e6c5a149
Devender0023 <kumar.devender0023@gmail.com>
2022-05-17 15:55:52 +1000

    removed files

--- catkin_ws/build/init_test/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dev/catkin_ws/src/init_test
-# Build directory: /home/dev/catkin_ws/build/init_test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
